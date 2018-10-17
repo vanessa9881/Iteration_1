b055d10a85ef1e5cdb9e5cdd89b27446c31401f9
Renato. S <brickorked@gmail.com>
2018-10-17 13:08:04 -0400

    Overrode Equals method for Tile
    Overrode .equals so that using tile1.equals(tile2) should check whether they have the same colour and value.

--- comp3004project/src/rummi/Tile.java
+++ comp3004project/src/rummi/Tile.java
@@ -11,9 +11,29 @@ public class Tile {
 		return 0;
 	}
 
-	public Object getColour() {
+	public String getColour() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+    public boolean equals(Object o) {
+		// Overrides the equals method for tile, should check if the colour
+		// and the value are the same!
+		
+		// If the object is compared with itself then return true   
+        if (o == this) { 
+            return true; 
+        } 
+  
+        // Check if compared object is a Tile
+        if (!(o instanceof Tile)) { 
+            return false; 
+        } 
+           
+        Tile t = (Tile) o;   
+        // Compare the char and int  
+        return (t.getValue() == this.getValue()) && (t.getColour().equals(this.getColour()));
 	}
 
 }
