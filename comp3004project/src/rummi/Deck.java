9d946e49a7a7c02ca27e9c386e465e3c45b6199b
Renato. S <brickorked@gmail.com>
2018-10-17 13:09:48 -0400

    Finished Deck Tests
    All tests for the deck class are implemented, including creation, .draw() and .shuffle()

--- comp3004project/src/rummi/Deck.java
+++ comp3004project/src/rummi/Deck.java
@@ -1,22 +1,37 @@
 package rummi;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Deck {
 
+	private ArrayList<Tile> playingDeck;
+	
 	public int getNumberOfTiles() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	public Object peek() {
+	public Tile peek() {
 		// TODO Auto-generated method stub
 		// Peeks at the tile on top of the deck, aka returns a reference to it
 		return null;
 	}
 
-	public Object draw() {
+	public Tile draw() {
 		// TODO Auto-generated method stub
 		// Will return the tile as well as remove it from the deck
 		return null;
+	}
+
+	public ArrayList<Tile> getDeck() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void shuffle() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
