7c702ca1c1141a46f8624e923d942461d6bec157
Renato S <brickworked@gmail.com>
2018-10-29 12:36:40 -0400

    Renamed Main Class
    Renamed main class and added initialization of game

---
+++ comp3004project/src/rummi/RummiMain.java
@@ -0,0 +1,10 @@
+package rummi;
+
+public class RummiMain {
+	
+	public static void main(String[] args) {
+		Game gameObject = new Game();
+		gameObject.play();
+		
+	}
+}
--- comp3004project/src/rummi/main.java
+++
@@ -1,5 +0,0 @@
-package rummi;
-
-public class main {
-
-}
