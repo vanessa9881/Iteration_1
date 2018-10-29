893ca0fcaeca88cdb42045917195b0ce340de844
Renato S <brickworked@gmail.com>
2018-10-29 12:39:28 -0400

    Game Constructor Created
    Created the constructor for class Game

--- comp3004project/src/rummi/Game.java
+++ comp3004project/src/rummi/Game.java
@@ -1,5 +1,24 @@
 package rummi;
 
+import java.util.ArrayList;
+
 public class Game {
+
+	Deck playingDeck;
+	ArrayList<Player> playerList;
+	
+	public Game() {
+		playingDeck = new Deck();
+		playerList = new ArrayList<Player>();
+		playerList.add(new HumanPlayer());
+		playerList.add(new Strategy1());
+		playerList.add(new Strategy2());
+		playerList.add(new Strategy2());
+	}
+	
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
