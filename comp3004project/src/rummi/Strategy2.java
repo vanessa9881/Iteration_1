3d7e6dd285bff8014a08472a7a6d327602bfa5f7
ryanmmitchell <ryanmmitchell@cmail.carleton.ca>
2018-11-02 14:48:11 -0400

    Testing fix and score check

--- comp3004project/src/rummi/Strategy2.java
+++ comp3004project/src/rummi/Strategy2.java
@@ -1,32 +1,42 @@
 package rummi;
 
+import java.util.*;
+
 public class Strategy2 extends Player {
 	
 	//Add constructors, etc... 
 	
 	
 	// Method to show if P3 can make new melds 
-	public boolean makeNewMelds(Player player) {
-		int handDifference = 0; 
+	public boolean makeNewMelds(ArrayList<Player> players) {
+		boolean makenew = false;
 		
-		if (player.getHandValue() > this.getHandValue()) {
-			return true; 
-		} else {
-			handDifference = this.getHandValue() - player.getHandValue();
-			if (handDifference >= 3) {
-				return false;
+		for (Player player : players) {
+			int handDifference = 0; 
+			if (player.equals(this)) {
+				makenew = makenew; 
+			} else if (player.getHandValue() > this.getHandValue()) {
+				makenew =  true; 
 			} else {
-				return true; 
+				handDifference = this.getHandValue() - player.getHandValue();
+				if (handDifference >= 3) {
+					makenew = false;
+				} else {
+					makenew = true; 
+				}
 			}
+			
 		}
+		return makenew;
 	}
 	
 	// Method for Turn
-	public void play(){
-		// if (this.maneNewMelds(players hand value))
-		// 		play turn without making new melds
-		// else
-		// 		play turn regular (code from player class)
+	public void play(Game g){
+		//if (this.maneNewMelds(playerlist)) {
+			//play turn without making new melds 
+		//} else {
+			//play turn regular (code from player class) 
+		//}
 		
 	}
 
--- comp3004project/src/testPackage/Strategy2Tests.java
+++ comp3004project/src/testPackage/Strategy2Tests.java
@@ -1,6 +1,7 @@
 package testPackage;
 
 import org.junit.Test;
+import java.util.*;
 
 import junit.framework.TestCase;
 import rummi.Deck;
@@ -10,17 +11,24 @@ import rummi.Strategy2;
 public class Strategy2Tests extends TestCase {
 	private Player testPlayerOne = new Player();
 	private Strategy2 testPlayerThree = new Strategy2();
+	private ArrayList<Player> players;
+	
+	
 	
 	
 	@Test
 	public void testTurn(){
+		players = new ArrayList<Player>();
+		players.add(testPlayerOne);
+		players.add(testPlayerThree);
+		
 		
 		testPlayerThree.setHandValue(14);
 		testPlayerOne.setHandValue(7);
-		assertEquals("This turn should only play off of board", false, testPlayerThree.makeNewMelds(testPlayerOne.getHandValue())); 
+		assertEquals("This turn should only play off of board", false, testPlayerThree.makeNewMelds(players)); 
 		
 		testPlayerThree.setHandValue(7);
 		testPlayerOne.setHandValue(14);
-		assertEquals("This turn should play new melds", true, testPlayerThree.makeNewMelds(testPlayerOne.getHandValue())); 
+		assertEquals("This turn should play new melds", true, testPlayerThree.makeNewMelds(players)); 
 	}
 }
