98f8bb24828066bd866a65793d196f58b0cbc271
Renato. S <brickorked@gmail.com>
2018-11-02 13:09:52 -0400

    Added overridable play method to Player
    Also began player sorting method

--- comp3004project/src/rummi/Player.java
+++ comp3004project/src/rummi/Player.java
@@ -1,12 +1,12 @@
 package rummi;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
 
 public class Player {
 	//To be done: Constructor with hand, values etc... 
 	private int handValue;
-	private List<Tile> hand;
+	private ArrayList<Tile> hand;
 	
 	public Player() {
 		this.hand = new ArrayList<Tile>();
@@ -15,6 +15,22 @@ public class Player {
 	
 	public int getHandValue() {
 		return this.handValue;
+	}
+	
+	public void sortHand() {
+		//Comparator cmp = new Comparator();
+	}
+
+	public void play() {
+		// To be overriden by each strategy
+		
+	}
+
+	public boolean checkWin() {
+		if (getHandValue() == 0) {
+			return true;
+		}
+		else {return false;}
 	}
 
 }
