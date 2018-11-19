09c6e8b712e7cd51999055f952774f8c68627aee
Renato S <brickworked@gmail.com>
2018-11-19 05:24:53 -0500

    BoardView Class Fix
    Added event handlers for different types of buttons and fixed errors

--- comp3004project/src/rummi/BoardView.java
+++ comp3004project/src/rummi/BoardView.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -13,6 +11,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -34,15 +33,11 @@ public class BoardView {
     private BoardController controller;
     private Board board;
     
-    EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+    EventHandler<ActionEvent> boardButtonPress = new EventHandler<ActionEvent>() {
     	public void handle(final ActionEvent e) {
+    		priorSelectedTile = selectedTile;
     		selectedTile = ((RummiButton) e.getSource()).getTile();
-    		if (controller.getHandTiles().contains(selectedTile)) {
-    			// Tile clicked on was in the hand
-    			priorSelectedTile = selectedTile;
-    			System.out.println("Selected hand tile: " + selectedTile.toString());
-    			return;
-    		}
+
     		if (selectedTile == null ) {
         		System.out.println("Selected board space is empty");
         		if (priorSelectedTile != null) {
@@ -55,12 +50,24 @@ public class BoardView {
                 	// so that if true that means the tile was added and that the board button
                 	// can have it's graphic changed
                 	controller.placeTile(x, y, priorSelectedTile);
-                	priorSelectedTile = selectedTile;
         		}
         	}
         	else {
         		System.out.println("Selected board space is occupied");
         	}
+    	}
+    };
+    
+    EventHandler<ActionEvent> drawTileButtonPress = new EventHandler<ActionEvent>() {
+    	public void handle(final ActionEvent e) {
+    	}
+    };
+    
+    EventHandler<ActionEvent> handButtonPress = new EventHandler<ActionEvent>() {
+    	public void handle(final ActionEvent e) {
+    		System.out.println("Selected hand tile: " + selectedTile.toString());
+    		priorSelectedTile = selectedTile;
+    		selectedTile = ((RummiButton) e.getSource()).getTile();
     	}
     };
     
@@ -89,49 +96,33 @@ public class BoardView {
 		gameBoard.setMaxSize(1150, 1000);
 	    BorderPane.setAlignment(gameBoard, Pos.TOP_LEFT);
 	    root.setCenter(gameBoard);	
-	    createGameBoardButtons();
+	    createBoardButtons();
 	}
     
-	 public void createHandButtons() {
-		handButtons = new Button[64];
+	public void createHandButtons() {
+		handButtons = new RummiButton[64];
 		//Always have more buttons than tiles or null
-		for (int i=0; i <= 64; i++) {
+		for (int i = 0; i < 64; i++) {
 			handButtons[i].setPrefSize(60, 60);
-			handButtons[i].setOnAction(new ButtonHandler(controller, handButtons[i]));
-			handButtons[i].setOnAction(new ButtonHandler());
+			handButtons[i].setOnAction(handButtonPress);
 			userPane.getChildren().add(handButtons[i]);			
 		}	
-			
-		for(int j=0;j<=63;j++) {
-			RummiButton myPlayerButton = playerHandButtons[j];
-			myPlayerButton.setPos(j,j);
-		    myPlayerButton.setOnAction(new EventHandler<ActionEvent>() {
-		        	
-		     // This part will need to be moved to its own class
-		            public void handle(ActionEvent event) {	            	
-		                String currentSelectedTileText;
-		            	System.out.println(myPlayerButton.getPos()[0] + "the index value selected in playerUserButton array");
-		            	System.out.println(playerHand.getNumberOfTiles() + " Tiles in the hand");
-		            	       	
-		            	if (myPlayerButton.getPos()[0] <= playerHand.getNumberOfTiles() -1 ) {
-		            		tileSelectedLabel.setText(playerHand.getTile(myPlayerButton.getPos()[0]).toString()); 
-		            		currentSelectedTile = playerHand.getTile(myPlayerButton.getPos()[0]);					// Set currently selected tile
-		            		currentSelectedButton = myPlayerButton;
-		            		System.out.println(currentSelectedTile.toString()+ " Current tile object selected"); 	            		
-		            	} else if (myPlayerButton.getPos()[0] > playerHand.getNumberOfTiles() - 1){
-		            		tileSelectedLabel.setText("No tile");	
-		            		currentSelectedTile = null;
-		            		if (currentSelectedTile == null) {
-		            		System.out.println("This is clicking on empty tile in hand after setting to null");
-		            		}
-		            	}
-		            	currentSelectedTileText = tileSelectedLabel.getText();
-		               	System.out.println(currentSelectedTileText + " Selected");
-		               	System.out.println("------------------------------------------------------------");
-		            }
-		        }); 
+	 }
+	
+	public void createBoardButtons() {
+		boardButtons = new ArrayList<RummiButton>();
+		for (int x = 0; x < 12; x++) {
+			for(int y = 0; y < 12; y++) {		
+				RummiButton newButton = new RummiButton(x,y);
+				newButton.setPrefSize(100, 100);
+				newButton.setOnAction(boardButtonPress);
+				GridPane.setHgrow(newButton, Priority.ALWAYS);
+				GridPane.setVgrow(newButton, Priority.ALWAYS);
+				gameBoard.add(newButton, x, y);	
+				boardButtons.add(newButton);
 			}
-	    }
+		}	
+	}
 	
     private VBox addVBox() {
         Button drawTileButton = new Button("Draw Tile");
@@ -146,11 +137,7 @@ public class BoardView {
         
         //Draw Tile Section
         drawTileButton.setMaxWidth(Double.MAX_VALUE);
-        drawTileButton.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-            	controller.drawTile(playerHand, playerHandButtons);
-            }
-        });
+        drawTileButton.setOnAction(drawTileButtonPress);
         
         //End Turn Section
         endTurnButton.setMaxWidth(Double.MAX_VALUE);
@@ -164,14 +151,9 @@ public class BoardView {
         moveInfoTextArea.setEditable(false);      
         return vbox;
     }
-
-	public Parent asParent() {
-		return view;
-	}
 	
 	public BoardController getController() {
 		return controller;
 	}
 	
-
 }
