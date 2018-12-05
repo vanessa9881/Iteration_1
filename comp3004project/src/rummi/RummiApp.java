59c4a5b101d54b6672ddfe0f8a719d2a5f359cd6
Manish4Star <sf.manish4@gmail.com>
2018-12-05 17:14:29 -0500

    adding start
    idk3

--- comp3004project/src/rummi/RummiApp.java
+++ comp3004project/src/rummi/RummiApp.java
@@ -1,14 +1,24 @@
 package rummi;
 
+import java.util.Scanner;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class RummiApp extends Application{
+	int getStrat1 =0;
+	int getStrat2 =0;
+	int getStrat3 =0;
+	int getStrat4 =0;
+	int getNumberHPlayers =0;
+	Boolean start = false;
 	
     @Override
     public void start(Stage primaryStage) {
     	
+    	initStart();
+    	
+    	if (start == true) {
         Board model = new Board();
         BoardView view = new BoardView(model);
 
@@ -16,9 +26,41 @@ public class RummiApp extends Application{
         primaryStage.setScene(scene);
         primaryStage.show();
         
-        model.setPlayers(0, 4);
+        model.setPlayers(getNumberHPlayers,getStrat1, getStrat2, getStrat3,getStrat4);
         model.startOrder();
+    	}
+        
 
+    }
+    public void initStart() {
+    	int totalNumber = 0;
+    	Scanner sc = new Scanner(System.in);
+    	
+		do {
+			totalNumber = 0;
+			System.out.println("Only 2-4 players combined");
+			System.out.println("Enter Number of Human Players: ");
+	    	getNumberHPlayers = sc.nextInt();
+	    	System.out.println("Enter Number of Strat 1: ");
+	    	getStrat1 = sc.nextInt();
+	    	System.out.println("Enter Number of Strat 2: ");
+	    	getStrat2 = sc.nextInt();
+	    	System.out.println("Enter Number of Strat 3: ");
+	    	getStrat3 = sc.nextInt();
+	    	System.out.println("Enter Number of Strat 4: ");
+	    	getStrat4 = sc.nextInt();
+	    	totalNumber = getNumberHPlayers + getStrat1 + getStrat2 + getStrat3 + getStrat4;
+		} while (totalNumber < 2 || totalNumber > 4);
+    	sc.close();
+
+    	
+
+    	
+    	/*
+    	 * if (enter is pressed){
+    	 * start = true;
+    	 * }
+    	 */
     }
     
 
